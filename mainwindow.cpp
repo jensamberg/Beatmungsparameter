a8ddb1fa9f680cb0bbeadc9d95bd5f39ec78914c
Jens Amberg <jensamberg@mail.de>
2018-04-22 20:52:45 +0200

    bring dpsupp to work

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -7,6 +7,8 @@
 #define PAWMAX   60 // Max 60 mbar
 #define PINSPMAX 55 // Max 55 mbar
 
+#define PINSP_TO_PEEP_MINLEVEL 5 // 5 mbar over PEEP
+
 
 
 MainWindow::MainWindow(QWidget *parent) :
@@ -73,6 +75,8 @@ void MainWindow::on_dPsupp_editingFinished()
 
 void MainWindow::on_PEEP_valueChanged(int arg1)
 {
+    qDebug() << "arg 1\r\n"<<arg1;
+
     qDebug() << "Edit PEEP einsteller Value Changed\r\n";
 
     // Read out all 3 parameters which are have dependecies for PEEP
@@ -82,7 +86,7 @@ void MainWindow::on_PEEP_valueChanged(int arg1)
     qDebug() << "Pinsp ="<<mActualpinsp<<"Paw"<<mActualpaw<<"dPsupp"<<mActualdpsupp;
 
     // Find the lowest number from
-    int lowernumber_peepmax = smallest(mActualpinsp-5,mActualpaw-1,(mActualpaw-mActualdpsupp)-1);
+    int lowernumber_peepmax = smallest(mActualpinsp-PINSP_TO_PEEP_MINLEVEL,mActualpaw-1,(mActualpaw-mActualdpsupp)-1);
 
     qDebug() << "Smallest is"<<lowernumber_peepmax;
 
@@ -92,23 +96,31 @@ void MainWindow::on_PEEP_valueChanged(int arg1)
 
     ui->PEEP->setMaximum(this->mMAXPEEP);
     ui->PEEP->setMinimum(this->mMINPEEP);
+
+    refreshMINMAXLcdDisplays(0);
 }
 
 void MainWindow::on_Pinsp_valueChanged(int arg1)
 {
+
+   qDebug() << "arg 1\r\n"<<arg1;
 
 
    qDebug() << "Edit Pinps einsteller Value Changed\r\n";
 
    this->readOutActualValues(1);
 
-   qDebug() << "Pinsp ="<<mActualpinsp<<"Paw"<<mActualpaw<<"dPsupp"<<mActualdpsupp;
+   qDebug() << "PEEP ="<<mActualpeep<<"Pinsp ="<<mActualpinsp<<"Paw"<<mActualpaw<<"dPsupp"<<mActualdpsupp;
 
    // Find the lowest number from
-   int lowernumber = smallest(99,mActualpaw-1,PINSPMAX);
+   this->mMAXPINSP  = smallest(mActualpaw - mActualdpsupp - 1,mActualpaw-1,PINSPMAX);
 
-   ui->Pinsp->setMaximum(lowernumber);
-   ui->Pinsp->setMinimum(mActualpeep+5);
+   this->mMINPINSP = mActualpeep+PINSP_TO_PEEP_MINLEVEL;
+
+   ui->Pinsp->setMaximum(this->mMAXPINSP);
+   ui->Pinsp->setMinimum(this->mMINPINSP);
+
+   refreshMINMAXLcdDisplays(0);
 
 }
 
@@ -123,13 +135,35 @@ void MainWindow::on_Paw_valueChanged(int arg1)
     // Find the lowest number from
     int lowernumber = smallest(PAWMAX,PAWMAX,PAWMAX);
 
+    this->mMAXPAW = lowernumber;
+    this->mMINPAW = mActualpinsp + mActualdpsupp + 1;
 
-    ui->Paw->setMaximum(lowernumber);
-    ui->Paw->setMinimum(mActualpinsp+1);
+
+    ui->Paw->setMaximum(this->mMAXPAW);
+    ui->Paw->setMinimum(this->mMINPAW);
+
+
+    refreshMINMAXLcdDisplays(0);
 }
 
 void MainWindow::on_dPsupp_valueChanged(int arg1)
 {
+   qDebug() << "arg 1\r\n"<<arg1;
+
+
+   this->readOutActualValues(0);
+
+   // Find the lowest number from
+   int lowernumber = smallest(PAWMAX,PAWMAX,PAWMAX);
+
+   this->mMAXDPSUPP = mActualpaw - mActualpinsp - 1;
+   this->mMINDPSUPP = 1;
+
+   ui->dPsupp->setMaximum(this->mMAXDPSUPP);
+   ui->dPsupp->setMinimum(this->mMINDPSUPP);
+
+
+   refreshMINMAXLcdDisplays(0);
 
 }
 
